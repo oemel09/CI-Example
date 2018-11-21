95622f10ae228c8d46f035ad61e568025a3868e4
oemel09 <github@oemel09.de>
2018-11-21 11:48:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# CI-Example
+Small example for continuous integration with travis-ci.com.  
+
+Tests run with JUnit 5 and JaCoCo is used to check code-coverage.  
+
+Results will be published to codecov.io.  
+
+## Badges
+These two services will also give you those nice, little badges. Enjoy!  
+[![Build Status](https://travis-ci.com/oemel09/CI-Example.svg?branch=master)](https://travis-ci.com/oemel09/CI-Example)
+[![codecov](https://codecov.io/gh/oemel09/CI-Example/branch/master/graph/badge.svg)](https://codecov.io/gh/oemel09/CI-Example)
---
+++ src/test/java/de/oemel09/ci/CalculatorTest.java
@@ -0,0 +1,61 @@
+package de.oemel09.ci;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+
+    private static Calculator calculator;
+
+    @BeforeAll
+    static void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testSum() {
+        assertEquals(4, calculator.sum(2, 2));
+        assertEquals(8, calculator.sum(4, 4));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(4, calculator.multiply(2, 2));
+        assertEquals(12, calculator.multiply(3, 4));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(2, calculator.subtract(4, 2));
+        assertEquals(2, calculator.subtract(8, 6));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2, calculator.divide(8, 4));
+        assertEquals(8, calculator.divide(16, 2));
+    }
+
+    @Test
+    void testDivideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(8, 0));
+    }
+
+    @Test
+    void testPow() {
+        assertEquals(8, calculator.pow(2, 3));
+    }
+
+    @Test
+    void testPowWithExponent1() {
+        assertEquals(2, calculator.pow(2, 1));
+    }
+
+    @Test
+    void testPowWithExponent0() {
+        assertEquals(1, calculator.pow(2, 0));
+    }
+}
