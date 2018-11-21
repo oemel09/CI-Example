f7da76f24c938bdef829627dcafd4b8b9ddf8ae0
oemel09 <github@oemel09.de>
2018-11-21 11:50:09 -0500

    add and multiply with tests

--- src/main/java/de/oemel09/ci/Calculator.java
+++ src/main/java/de/oemel09/ci/Calculator.java
@@ -9,21 +9,4 @@ class Calculator {
     int multiply(int a, int b) {
         return a * b;
     }
-
-    int subtract(int a, int b) {
-        return a - b;
-    }
-
-    int divide(int a, int b) {
-        if (b == 0) throw new IllegalArgumentException();
-        return a / b;
-    }
-
-    int pow(int base, int exponent) {
-        if (exponent == 0) return 1;
-        if (exponent > 1) {
-            return base * pow(base, exponent - 1);
-        }
-        return base;
-    }
 }
